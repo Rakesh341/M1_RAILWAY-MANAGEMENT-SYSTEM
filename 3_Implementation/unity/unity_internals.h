10ec81fa2a6dd9f7f41589e55216b6a6e7ee3601
Rakesh341 <62551088+Rakesh341@users.noreply.github.com>
2022-02-10 17:07:57 +0530

    Update unity_internals.h

--- 3_Implementation/unity/unity_internals.h
+++ 3_Implementation/unity/unity_internals.h
@@ -1,6 +1,6 @@
 /* ==========================================
     Unity Project - A Test Framework for C
-    Copyright (c) 2008-22 Mike Karlesky, Mark VanderVoord, Greg Williams
+    Copyright (c) 2007-21 Mike Karlesky, Mark VanderVoord, Greg Williams
     [Released under MIT License. Please refer to license.txt for details]
 ========================================== */
 
